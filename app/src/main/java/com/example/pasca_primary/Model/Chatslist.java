148e398da7d79fc96821100772774a7f6fc126d9
bereket <bereketsewnet306@gmail.com>
2023-03-20 17:13:06 +0300

    Initial commit  hellow

---
+++ app/src/main/java/com/example/pasca_primary/Model/Chatslist.java
@@ -0,0 +1,22 @@
+package com.example.pasca_primary.Model;
+
+public class Chatslist {
+
+    String id;
+
+    public Chatslist(String id) {
+        this.id = id;
+    }
+
+
+    public Chatslist() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
